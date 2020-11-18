2c7adb662ffb01150a91aeb1fd63b97ebc5afdb7
Hassan Mir <hassanmir92@gmail.com>
2020-11-18 21:51:49 +0000

    BookingSystem to initialize with array of booking requests texts

--- lib/booking_system.rb
+++ lib/booking_system.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 class BookingSystem
+  attr_reader :booking_requests_texts
+
+  def self.process(requests_file_path)
+    new(File.open(requests_file_path).read).process
+  end
+
+  def initialize(content)
+    @booking_requests_texts = content.split(/,\r\n/)
+  end
+
+  def process
+    self
+  end
+
   def number_of_rejected_bookings
     10
   end
--- test/booking_system_test.rb
+++ test/booking_system_test.rb
@@ -5,10 +5,16 @@ require_relative '../lib/booking_system'
 
 class BookingSystemTest < Minitest::Test
   def setup
-    @booking_system = BookingSystem.new
+    @booking_system = BookingSystem.process('test/fixtures/sample_booking_requests')
   end
 
   def test_number_of_rejected_bookings
     assert_equal 10, @booking_system.number_of_rejected_bookings
+  end
+
+  def test_booking_request_texts
+    assert_equal 500, @booking_system.booking_requests_texts.count
+    assert_equal '(0,47:39,47:41)', @booking_system.booking_requests_texts.first
+    assert_equal '(499,42:18,42:21)', @booking_system.booking_requests_texts.last
   end
 end
